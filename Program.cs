03da6e8a8bbeaeb8a07823f99ac7cd99c7c8037a
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:31:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿interface IComponent { void Operation(); }
+
+class ConcreteComponent : IComponent { public void Operation() => Console.WriteLine("Base operation"); }
+
+class Decorator : IComponent
+{
+    private IComponent _wrap;
+    public Decorator(IComponent c) { _wrap = c; }
+    public void Operation() { _wrap.Operation(); Console.WriteLine(" + Decorated"); }
+}
+
+class Program { static void Main() => new Decorator(new ConcreteComponent()).Operation(); }
